121a17aecf77742af47255f867e1bc14e9932602
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2022-11-05 02:01:47 -0400

    Update podscrape.py

--- podscrape.py
+++ podscrape.py
@@ -5,7 +5,7 @@ urls = []
 
 for entry in feed['entries']:
     for enclosure in entry.get('enclosures'):
-        urls.append(enclosure['href'])
+        urls.append(enclosure['url'])
 
 urls_file = open('urls.txt', 'a')
 
@@ -13,3 +13,4 @@ for url in urls:
     urls_file.write(f'{url}\n')
 
 urls_file.close()
+
