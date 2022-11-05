7123656520e0cb1630bee59e72a227b9d8e37bfc
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2022-11-05 02:01:07 -0400

    Update podscrape.sh

--- podscrape.sh
+++ podscrape.sh
@@ -9,16 +9,22 @@ mkdir $directory
 
 cd $directory
 
-wget -i ../urls.txt
+wget -i ../urls.txt --content-disposition
 
-find -type f -name '*teaser*' -delete
-find -type f -name '*TEASER*' -delete
-find -type f -name '*Teaser*' -delete
-find -type f -name '*preview*' -delete
-find -type f -name '*PREVIEW*' -delete
-find -type f -name '*Preview*' -delete
-find -type f -name '*Trailer*' -delete
-find -type f -name '*trailer*' -delete
-find -type f -name '*TRAILER*' -delete
+if [[ $url =~ "patreon" ]]; then
+   echo "Skipping filename cleaning."
+else
+   for i in *.mp3*; do mv "$i" "${i%\?*}"; done
+   find -type f -name '*teaser*' -delete
+   find -type f -name '*TEASER*' -delete
+   find -type f -name '*Teaser*' -delete
+   find -type f -name '*preview*' -delete
+   find -type f -name '*PREVIEW*' -delete
+   find -type f -name '*Preview*' -delete
+   find -type f -name '*Trailer*' -delete
+   find -type f -name '*trailer*' -delete
+   find -type f -name '*TRAILER*' -delete
+fi
 
-for i in *.mp3*; do mv "$i" "${i%\?*}"; done
+rm feed.xml
+rm urls.txt
